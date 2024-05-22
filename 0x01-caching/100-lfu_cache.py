265641535bc4d184a6139a2e8928f998f0faaaa2
faith2221 <edithbanda014@gmail.com>
2024-05-22 09:19:41 +0000

    Advanced Task

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -22,33 +22,46 @@ class LFUCache(BaseCaching):
         """
         Add an item in the cache using LFU strategy
         """
-        if key is not None and item is not None:
-            # Checks if cache is full
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                min_frequency = min(self.frequencies)
-                least_frequency_keys = [
-                    k for k, v in enumerate(self.frequencies)
-                    if v == min_frequency
-                ]
-                least_recently_used_key = min(
-                    least_frequent_keys,
-                    key=lambda k: self.cache_data[k].get("last_used"),
-                )
-                del self.cache_data[least_recently_used_key]
-                del self.frequencies[least_recently_used_key]
-                print("DISCARD:", least_recently_used_key)
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                lfu = min(self.frequency.values())
+                lfu_keys = []
+                for k, v in self.frequency.items():
+                    if v == lfu:
+                        lfu_keys.append(k)
+                if len(lfu_keys) > 1:
+                    lru_lfu = {}
+                    for k in lfu_keys:
+                        lru_lfu[k] = self.usage.index(k)
+                    discard = min(lru_lfu.values())
+                    discard = self.usage[discard]
+                else:
+                    discard = lfu_keys[0]
 
-            # Add the new item to cache
-            self.cache_data[key] = {"value": item, "last_used": 0}
-            self.frequencies.append(0)
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
+                del self.usage[self.usage.index(discard)]
+                del self.frequency[discard]
+            # update usage frequency
+            if key is self.frequency:
+                self.frequency[key] += 1
+            else:
+                self.frequency[key] = 1
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            self.cache_data[key]= item
 
     def get(self, key):
         """
-        Get an item from the cache by key.
+        Return the value linked to a given key, or None.
         """
-        if key in self.cache_data:
-            idx = list(self.cache_data.keys()).index(key)
-            self.frequencies[idx] += 1
-            self.cache_data[key]["last_used"] += 1
-            return self.cache_data[key]["value"]
+        if key is not None and key in self.cache_data.keys():
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            self.frequency[key] += 1
+            return self.cache_data[key]
         return None
