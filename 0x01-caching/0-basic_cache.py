a570991b3bf324558eea6b3b6391260e029f74fb
faith2221 <edithbanda014@gmail.com>
2024-05-22 06:46:24 +0000

    0. Basic dictionary and 1. FIFO caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+class BasicCache that inherits from BaseCaching and is a caching system.
+"""
+from base_caching import BaseCaching
+
+
+class BaseCache(BaseCaching):
+    """
+    Caching system.
+    """
+    def __init__(self):
+        """
+        Initialize.
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add data to cache.
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return value in self.cache_data linked to a key.
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
